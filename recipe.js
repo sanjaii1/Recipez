a29cde0891abf8b18a0e6ac430eaa1868d322a3d
Sanjai <bsanjay235@gmail.com>
2024-06-05 16:22:56 +0530

    event

--- recipe.js
+++ recipe.js
@@ -1,17 +1,15 @@
-
 function showTab(event, tabId) {
     event.preventDefault();
     var tabContents = document.querySelectorAll('.tab-pane');
     tabContents.forEach(function(tabContent) {
       tabContent.classList.remove('show', 'active');
     });
-  
+
     var tabLinks = document.querySelectorAll('.nav-link');
     tabLinks.forEach(function(tabLink) {
       tabLink.classList.remove('active');
     });
-  
+
     document.getElementById(tabId).classList.add('show', 'active');
     event.currentTarget.classList.add('active');
   }
-  
